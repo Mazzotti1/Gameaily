e26caf212427c321249cabf527915d354374359a
Gabriel Mazzotti <gmazzottidesouza@gmail.com>
2023-08-11 22:59:12 -0300

    Fragments and navbar

--- Whatsthegame/app/src/main/java/com/whatsthegame/MainActivity.kt
+++ Whatsthegame/app/src/main/java/com/whatsthegame/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.whatsthegame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.homeButtonStart)
+
+        startButton.setOnClickListener {
+            val intente = Intent(this, WhatsTheGameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
