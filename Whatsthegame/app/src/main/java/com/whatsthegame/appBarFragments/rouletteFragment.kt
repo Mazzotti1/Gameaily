d7b66abf1c98a1ccba5beeeb66e62f6f9c318f05
Gabriel Mazzotti <gmazzottidesouza@gmail.com>
2023-08-19 21:33:03 -0300

    add Roulette wheel

--- Whatsthegame/app/src/main/java/com/whatsthegame/appBarFragments/rouletteFragment.kt
+++ Whatsthegame/app/src/main/java/com/whatsthegame/appBarFragments/rouletteFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import com.whatsthegame.R
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +37,81 @@ class rouletteFragment : Fragment() {
         }
     }
 
+
+    private val sectors = arrayOf("Roxa", "Verde", "Azul", "Roxa", "Verde", "Azul", "Dourada", "Verde", "Azul", "Vermelha", "Verde", "Azul")
+    private val sectorDegrees = IntArray(sectors.size)
+    private val random = Random()
+    private var degree = 0
+    private var isSpinning = false;
+    private lateinit var rouletteRollBody: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_roulette, container, false)
+        val view = inflater.inflate(R.layout.fragment_roulette, container, false)
+
+        val rouletteRollButton = view?.findViewById<Button>(R.id.rouletteRollButton)
+        rouletteRollBody = view?.findViewById<ImageView>(R.id.rouletteBody)!!
+        getDegreeForSectors()
+
+
+        if (rouletteRollButton != null) {
+            rouletteRollButton.setOnClickListener { v ->
+                if (!isSpinning) {
+                    spin()
+                    isSpinning = true
+                }
+            }
+        }
+
+        return view
+    }
+    private fun spin() {
+        degree = random.nextInt(sectors.size - 1)
+
+        val rotateAnimation = RotateAnimation(
+            0f,
+            (360f * sectors.size) + sectorDegrees[degree],
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f,
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f
+        )
+
+        rotateAnimation.duration = 3600
+        rotateAnimation.fillAfter = true
+        rotateAnimation.interpolator = DecelerateInterpolator()
+        rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                Toast.makeText(
+                    requireContext(),
+                    "Você ganhou uma recompensa ${sectors[sectors.size - (degree + 1)]}",
+                    Toast.LENGTH_SHORT
+                ).show()
+                isSpinning = false;
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+
+            }
+        })
+
+        rouletteRollBody.startAnimation(rotateAnimation)
+    }
+
+
+    private fun getDegreeForSectors(){
+        val sectorDegree = 360/sectors.size
+
+        for (i in 0 until sectors.size) {
+            sectorDegrees[i] = (i + 1) * sectorDegree
+        }
+
     }
 
     companion object {
